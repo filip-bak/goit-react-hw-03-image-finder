0930ea4ba221e46f5b0e65949a249c814d9ede49
Filip Bączkowski <filo.b@wp.pl>
2023-07-13 23:09:51 +0200

    fix: component path

--- src/components/ImageGallery/index.jsx
+++ src/components/ImageGallery/index.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import ImageGalleryItem from './ImageGalleryItem';
+import ImageGalleryItem from './ImageGalleryItem/index';
 
 import styles from './ImageGallery.module.css';
 
